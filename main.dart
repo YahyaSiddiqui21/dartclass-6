bcb6c11355ec1e3c0c9e75729efe81bfb84eda6f
yahya <yahyasiddiqui146@gmail.com>
2023-09-29 15:48:36 +0500

    hello

--- main.dart
+++ main.dart
@@ -1,3 +1,105 @@
+import 'dart:io';
+
 void main() {
-  print("hello world");
+  while (true) {
+    print("Select an option:");
+    print("1. Length Conversion");
+    print("2. Temperature Conversion");
+    print("3. Area Conversion");
+    print("4. Weight Conversion");
+    print("5. Time Conversion");
+    print("6. Exit");
+    int option = int.parse(stdin.readLineSync()!);
+
+    if (option == 6) {
+      print("Exiting the program.");
+      break;
+    }
+
+    switch (option) {
+      case 1:
+        lengthConversion();
+        break;
+      case 2:
+        temperatureConversion();
+        break;
+      case 3:
+        areaConversion();
+        break;
+      case 4:
+        weightConversion();
+        break;
+      case 5:
+        timeConversion();
+        break;
+      default:
+        print("Invalid option. Please try again.");
+    }
+
+    print("Do you want to continue?");
+    print("1. Yes");
+    print("2. No");
+    int continueOption = int.parse(stdin.readLineSync()!);
+
+    if (continueOption != 1) {
+      print("Exiting the program.");
+      break;
+    }
+  }
 }
+
+void lengthConversion() {
+  print("Select a length conversion option:");
+  print("1. Meter to kilometers");
+  print("2. Kilometer to Meters");
+  print("3. Feet to Inches");
+  print("4. Inches to Feet");
+  print("5. Centimeter to Meter");
+  print("6. Meter to Centimeter");
+  int option = int.parse(stdin.readLineSync()!);
+
+  double result = 0.0;
+  switch (option) {
+    case 1:
+      // Implement Meter to kilometers conversion here
+      break;
+    case 2:
+      // Implement Kilometer to Meters conversion here
+      break;
+    case 3:
+      // Implement Feet to Inches conversion here
+      break;
+    case 4:
+      // Implement Inches to Feet conversion here
+      break;
+    case 5:
+      // Implement Centimeter to Meter conversion here
+      break;
+    case 6:
+      // Implement Meter to Centimeter conversion here
+      break;
+    default:
+      print("Invalid option. Please try again.");
+      return;
+  }
+
+  print("Result: $result");
+}
+
+// Implement similar functions for temperatureConversion, areaConversion, weightConversion, and timeConversion
+void temperatureConversion() {
+  // Implement temperature conversion here
+}
+
+void areaConversion() {
+  // Implement area conversion here
+}
+
+void weightConversion() {
+  // Implement weight conversion here
+}
+
+void timeConversion() {
+  // Implement time conversion here
+}
+
